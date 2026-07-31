ade554b548f134973404ef4a6b1e4365ddafa93f
Clinton <clinton@localhost.localdomain>
2026-07-31 14:07:25 +0200

    updated env file to change api port to avoid conflict

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   const isMock    = env.VITE_MOCK === 'true'
-  const apiTarget = env.VITE_API_URL ?? 'http://localhost:8080'
+  const apiTarget = env.VITE_API_URL ?? 'http://localhost:8082'
 
   return {
     plugins: [react()],
